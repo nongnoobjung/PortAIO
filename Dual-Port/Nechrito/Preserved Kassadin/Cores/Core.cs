88197d23fec61c078b2636a482ff5670623b756e
Pablo Sanchez <jrcrack009@gmail.com>
2016-06-24 10:10:27 -0600

    Preserved Kassadin [SDK] (Dual-Port)

---
+++ Dual-Port/Nechrito/Preserved Kassadin/Cores/Core.cs
@@ -0,0 +1,17 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using LeagueSharp.SDK;
+
+namespace Preserved_Kassadin.Cores
+{
+    class Coree
+    {
+        public static AIHeroClient Player => ObjectManager.Player;
+        public static AIHeroClient Target => TargetSelector.GetTarget(1400, DamageType.Magical);
+
+        public static bool SafeTarget(Obj_AI_Base target)
+        {
+            return target != null && target.LSIsValidTarget() && !target.IsDead && !target.IsInvulnerable;
+        }
+    }
+}
---
+++ Dual-Port/Nechrito/Preserved Kassadin/Program.cs
@@ -0,0 +1,14 @@
+﻿using LeagueSharp.SDK;
+using System;
+
+namespace Preserved_Kassadin
+{
+    class Program
+    {
+        public static void Load()
+        {
+            if (GameObjects.Player.ChampionName != "Kassadin") return;
+            LoadAssembly.Load();
+        }
+    }
+}
